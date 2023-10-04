b139222202e0bcb2a0210bcb5945da03a68ad809
bee <beebelam0@gmail.com>
2023-10-04 21:09:16 +0300

    home section progress

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Home from './Home'
 function App() {
   return (
     <div>
       <Navbar />
+      <section>
+        <Home />
+      </section>
+      
     </div>
   )
 }
---
+++ src/Components/Home.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function Home() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="box">
+      <div className="container-home">
+        <Slider {...settings}>
+          <div className="slider-item">
+            <div className="slider-content">
+              <img src="images/recipe2.jpeg" alt="not found" />
+              <p>Get our meals at affordable prices  <div className='buttons'>
+              <button>Order Now</button>
+              </div></p>
+             
+            </div>
+           
+          </div>
+          <div className="slider-item">
+            <div className="slider-content">
+              <img src="images/recipe3.jpeg" alt="" />
+              <p>Get our meals at affordable prices  <div className='buttons'>
+              <button>Order Now</button>
+              </div></p>
+            </div>
+          </div>
+          <div className="slider-item">
+            <div className="slider-content">
+              <img src="images/recipe4.jpeg" alt="" />
+              <p>Get our meals at affordable prices  <div className='buttons'>
+              <button>Order Now</button>
+              </div></p>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
